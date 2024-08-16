bc53e57af4703014784e2210867e2e2471f64f48
Juuso Tuononen <jutuon@outlook.com>
2024-08-16 21:44:45 +0300

    Update vergen

--- crates/app-manager/build.rs
+++ crates/app-manager/build.rs
@@ -1,17 +1,29 @@
-use vergen::EmitBuilder;
+use vergen_gitcl::{CargoBuilder, Emitter, GitclBuilder, RustcBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    EmitBuilder::builder()
+    Emitter::default()
+        .add_instructions(
+            &CargoBuilder::default()
+                .target_triple(true)
+                .debug(true)
+                .features(true)
+                .opt_level(true)
+                .build()?
+        )?
+        .add_instructions(
+            &RustcBuilder::default()
+                .semver(true)
+                .host_triple(true)
+                .build()?
+        )?
+        .add_instructions(
+            &GitclBuilder::default()
+                .branch(true)
+                .describe(true, true, None)
+                .sha(false)
+                .build()?
+        )?
         .fail_on_error()
-        .cargo_target_triple()
-        .cargo_debug()
-        .cargo_features()
-        .cargo_opt_level()
-        .rustc_semver()
-        .rustc_host_triple()
-        .git_branch()
-        .git_describe(true, true, None)
-        .git_sha(false)
         .emit()?;
 
     Ok(())
