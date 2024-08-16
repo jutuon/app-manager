73af3a12c73b99392ef25a016767e1c2e7bb9b3e
Juuso Tuononen <jutuon@outlook.com>
2024-08-16 21:15:09 +0300

    Fix API bindings
    Remove "extern crate" definitions and ignore unused imports.

--- crates/manager_api_client/src/lib.rs
+++ crates/manager_api_client/src/lib.rs
@@ -1,15 +1,8 @@
 #![allow(
     clippy::empty_docs,
     clippy::to_string_trait_impl,
+    unused_imports,
 )]
-
-#[macro_use]
-extern crate serde_derive;
-
-extern crate serde;
-extern crate serde_json;
-extern crate url;
-extern crate reqwest;
 
 pub mod apis;
 pub mod models;
